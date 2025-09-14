b3a71cb93b6919955c0bc431728c6b83dc6e15f5
CHATCHAWONG <chatchawong903@gmail.com>
2025-09-14 23:01:37 +0700

    add new menu

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,8 @@ exports.getHomePage = (req, res) => {
     homeImages: [
       "/images/1756817039328_ข้าวมันไก่.jpeg", 
       "/images/1756819888309_ไก่ย่าง.jpeg", 
-      "/images/1757056224321_ข้าวหมกไก่ทอด.jpeg"
+      "/images/1757056224321_ข้าวหมกไก่ทอด.jpeg",
+      "/images/1757864992367_ปูไข่นึ่งนมสด.jpeg",
     ] // ส่ง Array ของรูปภาพ
   });
 };
